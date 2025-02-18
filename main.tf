3821bc9a8cc8eed7de9ce7a5b6a7ed60086cd2e6
venkat-R-git <rvsaws2@gmail.com>
2025-02-18 14:00:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
 region = "us-east-1"
 }
-
 resource "aws_instance" "one" {
 count = 5
 ami = "ami-0ddc798b3f1a5117e"
@@ -12,7 +11,6 @@ tags = {
 Name = var.instance_names[count.index]
 }
 }
-
 variable "instance_names" {
 default = ["jenkins", "nexus", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
