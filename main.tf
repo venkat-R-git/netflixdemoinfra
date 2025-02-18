98470b50ad43398c72b59119e6f7d194b931e5f5
venkat-R-git <rvsaws2@gmail.com>
2025-02-18 13:41:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "one" {
 count = 5
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
-key_name = "rahull"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "terraform.pem"
+vpc_security_group_ids = ["sg-0425b3a4bedd78d69"]
 tags = {
 Name = var.instance_names[count.index]
 }
